b9f34e86373eefe2ccae3f6a2a8a55a36bf670ee
BV Satyaram <bvsatyaram@gmail.com>
2017-06-29 01:49:44 +0530

    Convert LangaugeSelector component into a stateless functional component

--- app/components/LanguageSelector.js
+++ app/components/LanguageSelector.js
@@ -1,24 +1,22 @@
 var React = require('react');
 
-class LanguageSelector extends React.Component {
-  render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-    
-    return (
-      <ul className='languages'>
-        {languages.map(function(lang) {
-          return (
-            <li
-              className={this.props.selectedLanguage === lang ? 'active' : ''}
-              onClick={this.props.selectLanugage.bind(null, lang)}
-              key={'li-' + lang}>
-              {lang}
-            </li>
-          );
-        }, this)}
-      </ul>
-    );
-  }
+function LanguageSelector(props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+
+  return (
+    <ul className='languages'>
+      {languages.map(function(lang) {
+        return (
+          <li
+            className={props.selectedLanguage === lang ? 'active' : ''}
+            onClick={props.selectLanugage.bind(null, lang)}
+            key={'li-' + lang}>
+            {lang}
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 module.exports = LanguageSelector;
