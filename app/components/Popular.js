26212a18818fb5ef561988ed1c36c6aff0af997d
BV Satyaram <bvsatyaram@gmail.com>
2017-07-08 01:11:26 +0530

    Fetch repos from API

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,23 +1,39 @@
 var React = require('react');
 var LanguageSelector = require('./LanguageSelector');
+var api = require('../utils/api');
 
 class Popular extends React.Component {
   constructor (props) {
     super(props);
 
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.selectLanugage = this.selectLanugage.bind(this);
   }
 
+  componentDidMount () {
+    this.selectLanugage(this.state.selectedLanguage);
+  }
+
   selectLanugage(lang) {
     this.setState(function(){
       return {
-        selectedLanguage: lang
+        selectedLanguage: lang,
+        repos: null
       };
     });
+
+    api.fetchPopularRepos(lang)
+      .then(function(repos) {
+        this.setState(function() {
+          return {
+            repos: repos
+          };
+        });
+      }.bind(this))
   }
 
   render() {
@@ -27,6 +43,8 @@ class Popular extends React.Component {
           selectedLanguage = {this.state.selectedLanguage}
           selectLanguage = {this.selectLanugage}
         />
+
+      {JSON.stringify(this.state.repos, null, 2)}
       </div>
     );
   }
