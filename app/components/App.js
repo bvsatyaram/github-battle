0c84dec3caec4953d2b09118d16ac42bbae0273d
BV Satyaram <bvsatyaram@gmail.com>
2017-07-09 03:03:10 +0530

    Add 404 Component

--- app/components/App.js
+++ app/components/App.js
@@ -2,11 +2,13 @@ var React = require('react');
 var ReactRouter = require('react-router-dom');
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 var Nav = require('./Nav');
 var Home = require('./Home');
 var Battle = require('./Battle');
 var Popular = require('./Popular');
+var FourOhFour = require('./FourOhFour');
 
 class App extends React.Component {
   render() {
@@ -21,9 +23,12 @@ class App extends React.Component {
       <Router basename={basePath}>
         <div className='container'>
           <Nav />
-          <Route exact path='/' component={Home} />
-          <Route path='/battle' component={Battle} />
-          <Route path='/popular' component={Popular} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route component={FourOhFour}/>
+          </Switch>
         </div>
       </Router>
     );
---
+++ app/components/FourOhFour.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+function FourOhFour() {
+  return(
+    <h1>404 - Not Found</h1>
+  );
+}
+
+module.exports = FourOhFour;
