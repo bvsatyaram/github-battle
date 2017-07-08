fc982a4c73fb83608fca60fec17204f96a1a3476
BV Satyaram <bvsatyaram@gmail.com>
2017-07-09 01:58:56 +0530

    Add components for Home and Battle

--- app/components/App.js
+++ app/components/App.js
@@ -4,6 +4,8 @@ var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 
 var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
 var Popular = require('./Popular');
 
 class App extends React.Component {
@@ -12,6 +14,8 @@ class App extends React.Component {
       <Router>
         <div className='container'>
           <Nav />
+          <Route exact path='/' component={Home} />
+          <Route path='/battle' component={Battle} />
           <Route path='/popular' component={Popular} />
         </div>
       </Router>
---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component {
+  render() {
+    return (
+      <div className='home-container'>
+        <h1>Battle.</h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Battle;
